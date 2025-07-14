5d52a5cee508f3e28392f4c6e976344919dc764e
ashish <ashishvishwakarma4562@gmail.com>
2025-07-14 17:04:31 +0530

    update 3

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ app.use(cors({
         "https://user-new.onrender.com",
       "  https://shubprabhat-userside2.onrender.com",
       "https://shubh-prabhat-sgsj.vercel.app",
-      "https://shubprabhat-userside2.onrender.com"
+      "https://shubprabhat-userside2.onrender.com",
+      "https://shubprabhat-userside3.onrender.com"
 
  
     ],
